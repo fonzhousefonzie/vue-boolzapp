c1078fd4de8f299e305731478306de05acdbe98b
fonzhousefonzie <harth.fonz@gmail.com>
2022-06-15 13:00:45 +0200

    milestone 3

---
+++ milestone3/main.js
@@ -0,0 +1,233 @@
+const contatti = [
+  {
+    name: "Michele",
+    avatar: "_1",
+    visible: true,
+    messages: [
+      {
+        date: "10/01/2020 15:30:55",
+        message: "Hai portato a spasso il cane?",
+        status: "sent",
+      },
+      {
+        date: "10/01/2020 15:50:00",
+        message: "Ricordati di stendere i panni",
+        status: "sent",
+      },
+      {
+        date: "10/01/2020 16:15:22",
+        message: "Tutto fatto!",
+        status: "received",
+      },
+    ],
+  },
+  {
+    name: "Fabio",
+    avatar: "_2",
+    visible: false,
+    messages: [
+      {
+        date: "20/03/2020 16:30:00",
+        message: "Ciao come stai?",
+        status: "sent",
+      },
+      {
+        date: "20/03/2020 16:30:55",
+        message: "Bene grazie! Stasera ci vediamo?",
+        status: "received",
+      },
+      {
+        date: "20/03/2020 16:35:00",
+        message: "Mi piacerebbe ma devo andare a fare la spesa.",
+        status: "sent",
+      },
+    ],
+  },
+  {
+    name: "Samuele",
+    avatar: "_3",
+    visible: false,
+    messages: [
+      {
+        date: "28/03/2020 10:10:40",
+        message: "La Marianna va in campagna",
+        status: "received",
+      },
+      {
+        date: "28/03/2020 10:20:10",
+        message: "Sicuro di non aver sbagliato chat?",
+        status: "sent",
+      },
+      {
+        date: "28/03/2020 16:15:22",
+        message: "Ah scusa!",
+        status: "received",
+      },
+    ],
+  },
+  {
+    name: "Alessandro B.",
+    avatar: "_4",
+    visible: false,
+    messages: [
+      {
+        date: "10/01/2020 15:30:55",
+        message: "Lo sai che ha aperto una nuova pizzeria?",
+        status: "sent",
+      },
+      {
+        date: "10/01/2020 15:50:00",
+        message: "Si, ma preferirei andare al cinema",
+        status: "received",
+      },
+    ],
+  },
+  {
+    name: "Alessandro L.",
+    avatar: "_5",
+    visible: false,
+    messages: [
+      {
+        date: "10/01/2020 15:30:55",
+        message: "Ricordati di chiamare la nonna",
+        status: "sent",
+      },
+      {
+        date: "10/01/2020 15:50:00",
+        message: "Va bene, stasera la sento",
+        status: "received",
+      },
+    ],
+  },
+  {
+    name: "Claudia",
+    avatar: "_6",
+    visible: false,
+    messages: [
+      {
+        date: "10/01/2020 15:30:55",
+        message: "Ciao Claudia, hai novità?",
+        status: "sent",
+      },
+      {
+        date: "10/01/2020 15:50:00",
+        message: "Non ancora",
+        status: "received",
+      },
+      {
+        date: "10/01/2020 15:51:00",
+        message: "Nessuna nuova, buona nuova",
+        status: "sent",
+      },
+    ],
+  },
+  {
+    name: "Federico",
+    avatar: "_7",
+    visible: false,
+    messages: [
+      {
+        date: "10/01/2020 15:30:55",
+        message: "Fai gli auguri a Martina che è il suo compleanno!",
+        status: "sent",
+      },
+      {
+        date: "10/01/2020 15:50:00",
+        message: "Grazie per avermelo ricordato, le scrivo subito!",
+        status: "received",
+      },
+    ],
+  },
+  {
+    name: "Davide",
+    avatar: "_8",
+    visible: false,
+    messages: [
+      {
+        date: "10/01/2020 15:30:55",
+        message: "Ciao, andiamo a mangiare la pizza stasera?",
+        status: "received",
+      },
+      {
+        date: "10/01/2020 15:50:00",
+        message: "No, l'ho già mangiata ieri, ordiniamo sushi!",
+        status: "sent",
+      },
+      {
+        date: "10/01/2020 15:51:00",
+        message: "OK!!",
+        status: "received",
+      },
+    ],
+  },
+];
+
+let today = new Date();
+
+function formattedDate(){
+  let day = today.getDate();
+  let month = today.getMonth();
+  const year = today.getFullYear();
+  let hour = today.getHours();
+  let minutes = today.getMinutes();
+  let seconds = today.getSeconds();
+  if(day < 10){
+    day = "0" + day;
+  };
+  if(month < 10){
+    month = "0" + month;
+  };
+  if(hour < 10){
+    hour = "0" + hour;
+  };
+  if(minutes < 10){
+    minutes = "0" + minutes;
+  };
+  if(seconds < 10){
+    seconds = "0" + seconds;
+  };
+  return day + '/' + month + '/' + year + " " + hour + ":" + minutes + ":" + seconds ;
+};
+
+const newVue = new Vue({
+
+  el: "#app",
+  data:{
+    contacts : contatti,
+    newMessage: "",
+    contact : "x",
+  },
+  methods:{
+    visible(i){
+      const th = this;
+      for (let x = 0; x < this.contacts.length; x++){
+        th.contacts[x].visible = false;
+      }
+      this.contacts[i].visible = true;
+    },
+    sendMessage(contactObject){
+      today = new Date();
+      const currentTime = formattedDate();
+      
+      contactObject.messages.push({
+        date: currentTime,
+        message: this.newMessage,
+        status: "sent",
+      });
+      this.newMessage = "";
+      this.contact = contactObject;
+    },
+    pushReceivedMessage(){
+      const th = this;
+      setTimeout(function(){
+        today = new Date();
+        const currentTime = formattedDate();
+
+        th.contact.messages.push({
+          date: currentTime,
+          message: "ok",
+          status: "received",
+      })}, 1000);
+  },
+},
+});
